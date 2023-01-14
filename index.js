5325e7e1c14decde3017e368e56f1da7bd79d205
codingmd <21052985+codingmd@users.noreply.github.com>
2023-01-14 07:01:09 -0800

    add player entry method

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const game = {
 	playerO: [],
 	playerX: [],
+	entries: 0,
 	gameOver: false,
 
 	tictactoe(playerKey, gameSquare) {
-		return false;
+		let validTurn = false;
+		if (playerKey == 0) {
+			validTurn = this.addO(gameSquare)
+		}
+
+
+		return validTurn;
 	},
+
+	addO(gameSquare) {
+		if (this.entries < 9) {
+			this.playerO.push(gameSquare);
+			this.entries++;
+		}
+	}
 };
 
 module.exports = game;
