d39f49f9e9c6b04c76dae35abb246ab4dc92d4d8
Emma Gale <emmajgale@hotmail.com>
2023-01-14 15:17:39 +0000

    add in square open check to see if player can take this square

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ const game = {
 	gameOver: false,
 
 	tictactoe(playerKey, gameSquare) {
-		let validTurn = false;
-		if (playerKey == 0) {
-			validTurn = this.addO(gameSquare)
-		}
-
+		let validTurn = this.takeTurn(playerKey, gameSquare);
 
 		return validTurn;
 	},
 
-	addO(gameSquare) {
-		if (this.entries < 9) {
-			this.playerO.push(gameSquare);
+	takeTurn(playerKey, gameSquare) {
+		if (this.entries < 9 && this.squareOpen(gameSquare)) {
+			if (playerKey == 0) {
+				this.playerO.push(gameSquare);
+			} else {
+				this.playerX.push(gameSquare);
+			}
 			this.entries++;
+			return true;
 		}
-	}
+
+		return false;
+	},
+
+	squareOpen(gameSquare) {
+		return (
+			!this.playerO.includes(gameSquare) && !this.playerX.includes(gameSquare)
+		);
+	},
 };
 
 module.exports = game;
